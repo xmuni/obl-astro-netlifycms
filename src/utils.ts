f90f164a700c7705206746143e90cc7325f6ba3c
xmuni <vitto.m93@gmail.com>
2023-01-10 11:56:18 +0100

    Fixed pathname matching

---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+export function matchesPathname(path: string, pathname: string) {
+    // console.log('Comparing pathname:', {path,pathname})
+    if(pathname.endsWith('/')) {
+        return pathname === path || pathname === path+'/'
+    }
+    else {
+        return pathname === path
+    }
+}
